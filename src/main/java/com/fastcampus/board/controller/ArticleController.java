beb8aae3e2a9ad96dd5d31f360408c5944ee6d49
Dongmin Kim <ddmkim94@gmail.com>
2023-11-21 16:40:50 +0900

    Test: 게시글 실패 테스트 작성
    미리 만들어둔 엔드 포인트를 보고 실패 테스트를 작성함.

---
+++ src/main/java/com/fastcampus/board/controller/ArticleController.java
@@ -0,0 +1,14 @@
+package com.fastcampus.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/articles")
+@RequiredArgsConstructor
+public class ArticleController {
+
+
+
+}
