c7168db0a310e61f130b2549c68ac9d400f18070
Dongmin Kim <ddmkim94@gmail.com>
2023-11-21 11:09:16 +0900

    Feat: 도메인 코드를 엔티티로 변환
    1개의 도메인 클래스를 1개의 테이블에 매칭되도록 설계했다.

--- src/main/java/com/fastcampus/board/domain/Article.java
+++ src/main/java/com/fastcampus/board/domain/Article.java
@@ -1,15 +1,85 @@
 package com.fastcampus.board.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@Builder
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter
+    @Column(nullable = false)
     private String title;
+
+    @Setter @Column(nullable = false, length = 10000)
     private String content;
+
+    @Setter
     private String hashtag;
 
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
     private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false, length = 100)
     private String createdBy;
+
+    @Column(nullable = false)
+    @LastModifiedDate
     private LocalDateTime modifiedAt;
+
+    @Column(nullable = false, length = 100)
+    @LastModifiedBy
     private String modifiedBy;
+
+    public static Article of(String title, String content, String hashtag) {
+        return Article.builder()
+                .title(title)
+                .content(content)
+                .hashtag(hashtag)
+                .build();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return Objects.equals(id, article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/com/fastcampus/board/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/board/domain/ArticleComment.java
@@ -1,14 +1,74 @@
 package com.fastcampus.board.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Builder
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class ArticleComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long articleId;
+
+    @Setter
+    @ManyToOne(optional = false)
+    private Article article;
+
+    @Setter
+    @Column(nullable = false, length = 500)
     private String content;
 
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
     private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false, length = 100)
     private String createdBy;
+
+    @Column(nullable = false)
+    @LastModifiedDate
     private LocalDateTime modifiedAt;
+
+    @Column(nullable = false, length = 100)
+    @LastModifiedBy
     private String modifiedBy;
+
+    public static ArticleComment of(Article article, String content) {
+        return ArticleComment.builder()
+                .article(article)
+                .content(content)
+                .build();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment that)) return false;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
