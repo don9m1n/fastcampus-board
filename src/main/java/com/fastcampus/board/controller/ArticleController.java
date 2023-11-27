546ace8aeeae2e0a62bcc2af2f5602ff0d9d58d0
Dongmin Kim <ddmkim94@gmail.com>
2023-11-27 14:52:51 +0900

    Feat: 검색 기능 구현
    option에 표현될 키워드들을 서버에서 내려주기 위해
    SearchType 리스트들을 model에 포함시켜서 전달하는 코드 추가

--- src/main/java/com/fastcampus/board/controller/ArticleController.java
+++ src/main/java/com/fastcampus/board/controller/ArticleController.java
@@ -38,6 +38,8 @@ public class ArticleController {
         List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
         model.addAttribute("articles", articles);
         model.addAttribute("paginationBarNumbers", barNumbers);
+        model.addAttribute("searchTypes", SearchType.values());
+
         return "articles/index";
     }
 
